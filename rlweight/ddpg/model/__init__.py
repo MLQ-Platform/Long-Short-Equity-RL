de5d17b74af17a28209dd65b94ddbcae2ac5cbc6
Yang-Hyun-Jun <sugumy@naver.com>
2025-03-16 13:02:06 +0900

    delete: simhash

--- rlweight/ddpg/model/__init__.py
+++ rlweight/ddpg/model/__init__.py
@@ -1,6 +1,5 @@
 from rlweight.ddpg.model.actor import Actor
 from rlweight.ddpg.model.critic import Critic
 from rlweight.ddpg.model.config import ModelConfig
-from rlweight.ddpg.model.simhash import SimHash
 
-__all__ = ["Actor", "Critic", "ModelConfig", "SimHash"]
+__all__ = ["Actor", "Critic", "ModelConfig"]
