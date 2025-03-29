bb5e48d3eec33b13da8c60e6d107f315045f76ef
Yang-Hyun-Jun <sugumy@naver.com>
2025-03-29 21:07:21 +0900

    chore: activation func

--- rlweight/ddpg/model/actor.py
+++ rlweight/ddpg/model/actor.py
@@ -17,7 +17,7 @@ class Actor(nn.Module):
             nn.Linear(128, 64),
             nn.ReLU(),
             nn.Linear(64, config.num_tickers),
-            nn.Sigmoid(),
+            nn.Tanh(),
         )
 
     def forward(self, state: torch.Tensor):
